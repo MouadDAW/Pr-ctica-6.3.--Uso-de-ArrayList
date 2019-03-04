67968a5495c608578c8feb930f1e674beca47ed6
Mouad <mouad.maimouni@gmail.com>
2019-03-04 16:50:46 +0100

    Métodos Getters y Setters,toString;

--- src/empresa/Vehiculo.java
+++ src/empresa/Vehiculo.java
@@ -10,5 +10,74 @@ package empresa;
  * @author Sara
  */
 public class Vehiculo {
-    
+
+    private String marca;
+    private String modelo;
+    private String color;
+    private String matricula;
+    private double tarifa;
+    private boolean disponible;
+
+    public Vehiculo(String marca, String modelo, String color, String matricula, double tarifa, boolean disponible) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.color = color;
+        this.matricula = matricula;
+        this.tarifa = tarifa;
+        this.disponible = disponible;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public double getTarifa() {
+        return tarifa;
+    }
+
+    public void setTarifa(double tarifa) {
+        this.tarifa = tarifa;
+    }
+
+    public boolean isDisponible() {
+        return disponible;
+    }
+
+    public void setDisponible(boolean disponible) {
+        this.disponible = disponible;
+    }
+
+    @Override
+    public String toString() {
+        return "Vehiculo{" + "marca=" + marca + ", modelo=" + modelo + ", color=" + color + ", matricula=" + matricula + ", tarifa=" + tarifa + ", disponible=" + disponible + '}';
+    }
+
 }
