5db6f4ccfc4183e969418c279eba63a78461f94d
Mouad <mouad.maimouni@gmail.com>
2019-03-04 23:49:04 +0100

    registrarCliente

--- src/empresa/EmpresaAlquiler.java
+++ src/empresa/EmpresaAlquiler.java
@@ -5,10 +5,100 @@
  */
 package empresa;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Mouad
  */
 public class EmpresaAlquiler {
-    
+
+    private String cif;
+    private String nombre;
+    private int totalClientes;
+
+    public EmpresaAlquiler(String cif, String nombre, int totalClientes, ArrayList<Cliente> clientes, int totalVehiculos, ArrayList<Vehiculo> vehiculos, int totalAlquileres, ArrayList<VehiculoAlquilado> alquileres) {
+        this.cif = cif;
+        this.nombre = nombre;
+        this.totalClientes = totalClientes;
+        this.clientes = clientes;
+        this.totalVehiculos = totalVehiculos;
+        this.vehiculos = vehiculos;
+        this.totalAlquileres = totalAlquileres;
+        this.alquileres = alquileres;
+    }
+    private ArrayList<Cliente> clientes;
+    private int totalVehiculos;
+    private ArrayList<Vehiculo> vehiculos;
+    private int totalAlquileres;
+    private ArrayList<VehiculoAlquilado> alquileres;
+
+    public String getCif() {
+        return cif;
+    }
+
+    public void setCif(String cif) {
+        this.cif = cif;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getTotalClientes() {
+        return totalClientes;
+    }
+
+    public void setTotalClientes(int totalClientes) {
+        this.totalClientes = totalClientes;
+    }
+
+    public ArrayList<Cliente> getClientes() {
+        return clientes;
+    }
+
+    public void setClientes(ArrayList<Cliente> clientes) {
+        this.clientes = clientes;
+    }
+
+    public int getTotalVehiculos() {
+        return totalVehiculos;
+    }
+
+    public void setTotalVehiculos(int totalVehiculos) {
+        this.totalVehiculos = totalVehiculos;
+    }
+
+    public ArrayList<Vehiculo> getVehiculos() {
+        return vehiculos;
+    }
+
+    public void setVehiculos(ArrayList<Vehiculo> vehiculos) {
+        this.vehiculos = vehiculos;
+    }
+
+    public int getTotalAlquileres() {
+        return totalAlquileres;
+    }
+
+    public void setTotalAlquileres(int totalAlquileres) {
+        this.totalAlquileres = totalAlquileres;
+    }
+
+    public ArrayList<VehiculoAlquilado> getAlquileres() {
+        return alquileres;
+    }
+
+    public void setAlquileres(ArrayList<VehiculoAlquilado> alquileres) {
+        this.alquileres = alquileres;
+    }
+
+        public void registrarCliente(Cliente nuevo) {
+        this.clientes.add(nuevo);
+
+    }
 }
