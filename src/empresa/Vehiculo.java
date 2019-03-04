8ee467e5a2cb08145192faf6405d41887fd76a73
Mouad <mouad.maimouni@gmail.com>
2019-03-04 21:56:28 +0100

    Método estático, en la clase Vehículo, que genere un Vehículo de forma aleatoria.

--- src/empresa/Vehiculo.java
+++ src/empresa/Vehiculo.java
@@ -5,6 +5,9 @@
  */
 package empresa;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 /**
  *
  * @author Mouad
@@ -18,7 +21,7 @@ public class Vehiculo {
     private double tarifa;
     private boolean disponible;
 
-    public Vehiculo(String marca, String modelo, String color, String matricula, double tarifa, boolean disponible) {
+    public Vehiculo(String matricula, String marca, String modelo, String color, double tarifa, boolean disponible) {
         this.marca = marca;
         this.modelo = modelo;
         this.color = color;
@@ -80,4 +83,71 @@ public class Vehiculo {
         return "Vehiculo{" + "marca=" + marca + ", modelo=" + modelo + ", color=" + color + ", matricula=" + matricula + ", tarifa=" + tarifa + ", disponible=" + disponible + '}';
     }
 
+    public static Vehiculo VehiculoAleatorio() {
+        Random random = new Random();
+
+        ArrayList<String> marca = new ArrayList<>();
+        marca.add("Audi");
+        marca.add("BMW");
+        marca.add("Seat");
+        marca.add("Ferrari");
+        marca.add("Ford");
+        marca.add("Mercedes");
+        marca.add("Dacia");
+        marca.add("Mini");
+        marca.add("Renault");
+
+        ArrayList<String> modelo = new ArrayList<>();
+        modelo.add("A3");
+        modelo.add("M5");
+        modelo.add("Leon");
+        modelo.add("Carrera");
+        modelo.add("Fiesta");
+        modelo.add("Benz");
+        modelo.add("Ducker");
+        modelo.add("Coper");
+        modelo.add("Clio");
+
+        boolean disponibilidad;
+
+        if (random.nextBoolean()) {
+            disponibilidad = true;
+        } else {
+            disponibilidad = false;
+        }
+
+        ArrayList<String> color = new ArrayList<>();
+        color.add("Rojo");
+        color.add("Negro");
+        color.add("Blanco");
+        color.add("Verde");
+        color.add("Amarillo");
+        color.add("Azul");
+        color.add("Naranja");
+
+        String matricula = "";
+        for (int i = 0; i < 7; i++) {
+
+            if (i < 4) {
+
+                matricula += random.nextInt(10);
+            } else {
+
+                matricula += (char) (random.nextInt(90 - 65 + 1) + 65);
+            }
+        }
+        double Tarifa;
+        Tarifa = random.nextInt(200 - 150 + 1) + 150;
+
+        String marcaAle = marca.get(random.nextInt(marca.size()));
+        String modeloAle = modelo.get(random.nextInt(modelo.size()));
+        String colorAle = color.get(random.nextInt(color.size()));
+        Vehiculo vehiculo = new Vehiculo(matricula, marcaAle, modeloAle, colorAle, Tarifa, disponibilidad);
+        System.out.println(vehiculo);
+        return vehiculo;
+    }
+
+    public static void main(String[] args) {
+        Vehiculo.VehiculoAleatorio();
+    }
 }
